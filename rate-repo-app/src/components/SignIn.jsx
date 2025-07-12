03eda1dfc0a8e93fb212dd4877832295c59130fa
hinmiro <miro.hintikka@gmail.com>
2025-07-12 10:40:25 +0300

    Add assignment 10.8

--- rate-repo-app/src/components/SignIn.jsx
+++ rate-repo-app/src/components/SignIn.jsx
@@ -1,7 +1,68 @@
-import { Text } from "react-native";
+import { Text, TextInput, Pressable, View, StyleSheet } from 'react-native'
+import { useFormik } from 'formik'
+import theme from '../theme'
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
-};
+    const style = StyleSheet.create({
+        formContainer: {
+            display: 'flex',
+            margin: 10,
+        },
+        inputContainer: {
+            marginBottom: 15,
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 7
+        },
+        signButton: {
+            padding: 10,
+            backgroundColor: 'blue',
+            borderRadius: 6,
+            display: 'flex',
+            alignSelf: 'flex-end',
+            alignItems: 'center',
+        },
+        buttonText: {
+            fontWeight: theme.fontWeights.bold,
+            fontSize: theme.fontSizes.subheading,
+            color: theme.colors.textSecondary
+        }
+    })
 
-export default SignIn;
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        onSubmit: (values) => {
+            console.log(values)
+        },
+    })
+
+    return (
+        <>
+            <View style={style.formContainer}>
+                    <TextInput
+                        placeholder="username"
+                        value={formik.values.username}
+                        onChangeText={formik.handleChange('username')}
+                        style={style.inputContainer}
+                    />
+                    <TextInput
+                        placeholder="password"
+                        secureTextEntry
+                        value={formik.values.password}
+                        onChangeText={formik.handleChange('password')}
+                        style={style.inputContainer}
+                    />
+                <View style={style.signButton}>
+                    <Pressable onPress={formik.handleSubmit}>
+                        <Text style={style.buttonText}>Sign in</Text>
+                    </Pressable>
+                </View>
+            </View>
+        </>
+    )
+}
+
+export default SignIn
