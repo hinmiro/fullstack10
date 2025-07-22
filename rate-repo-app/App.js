0e460fd2bd628013c83df237ef54433fa5b995e1
hinmiro <miro.hintikka@gmail.com>
2025-07-22 21:26:51 +0300

    Fix eslint errors

--- rate-repo-app/App.js
+++ rate-repo-app/App.js
@@ -2,12 +2,10 @@ import { NativeRouter } from 'react-router-native'
 import { ApolloProvider } from '@apollo/client'
 import Main from './src/components/Main'
 import createApolloClient from './src/utils/apolloClient'
-import Constants from 'expo-constants'
 
 const apolloClient = createApolloClient()
 
 const App = () => {
-    console.log(Constants.expoConfig)
 
     return (
         <NativeRouter
